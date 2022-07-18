06ead9f10f9e40e9cec30578b7038db34c7333ad
André Godeiro <andrevictor.godeiro@gmail.com>
2022-07-17 21:48:28 -0300

    [M1S03] Ex 4 - Projeto: iniciando

---
+++ week003/Projeto Lista de Convidados/script.js
@@ -0,0 +1,38 @@
+const convidados = [
+  {
+    nome: "André",
+    sobrenome: "Godeiro",
+    idade: 24,
+    setor: "Camarote",
+  },
+  {
+    nome: "Jax",
+    sobrenome: "Teller",
+    idade: 35,
+    setor: "Pista",
+  },
+  {
+    nome: "Marshall",
+    sobrenome: "Eriksen",
+    idade: 43,
+    setor: "Camarote",
+  },
+  {
+    nome: "Jesse",
+    sobrenome: "Pinkman",
+    idade: 38,
+    setor: "Arquibancada",
+  },
+  {
+    nome: "Chibs",
+    sobrenome: "Telford",
+    idade: 47,
+    setor: "Arquibancada",
+  },
+  {
+    nome: "Abel",
+    sobrenome: "Teller",
+    idade: 15,
+    setor: "Pista",
+  },
+];
