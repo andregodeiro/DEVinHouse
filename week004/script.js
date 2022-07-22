c2dc57dd616891ed05131cd7a7fcdda97a4ad135
André Godeiro <andrevictor.godeiro@gmail.com>
2022-07-22 19:23:22 -0300

    [M1S04] Ex 3 - Conta bancaria Parte 3

---
+++ week004/script.js
@@ -0,0 +1,40 @@
+let contasClientes = [
+  {
+    id: 1,
+    nome: "Cliente 01",
+    saldo: 500,
+  },
+  {
+    id: 2,
+    nome: "Cliente 02",
+    saldo: 3000,
+  },
+  {
+    id: 3,
+    nome: "Cliente 03",
+    saldo: 5000,
+  },
+];
+
+const selectConta = document.getElementById("conta");
+const body = document.body;
+
+const adicionaOpcao = (value, text) => {
+  const option = document.createElement("option");
+  option.value = value;
+  option.textContent = "text;
+
+  selectConta.appendChild(option);
+};
+
+const populaSelect = () => {
+  selectConta.innerHTML = "";
+  adicionaOpcao('0','(Selecione)');
+
+  contasClientes.forEach((conta) => {
+    adicionaOpcao(conta.id, conta.nome)
+
+  });
+};
+
+body.onload = populaSelect;
