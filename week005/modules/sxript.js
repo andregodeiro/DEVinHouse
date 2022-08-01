a0e792bbac997c3d18df51be1f0b152ca46a86e3
André Godeiro <andrevictor.godeiro@gmail.com>
2022-08-01 17:07:53 -0300

    [M1S05] - Ex 3 - Criando classes

---
+++ week005/modules/sxript.js
@@ -0,0 +1,12 @@
+class Produto {
+  nome;
+  preco;
+  emEstoque;
+  quatidade;
+  constructo(nome, preco, emEstoque, quantiade) {
+    this.nome = nome;
+    this.preco = preco;
+    this.emEstoque = emEstoque;
+    this.quantidade = quantidade;
+  }
+}
