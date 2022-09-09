5ccad94e542d826776acec2c7f72f2e660963d1b
André Godeiro <andrevictor.godeiro@gmail.com>
2022-09-09 19:34:50 -0300

    [M1S11] Ex 1 - Setup Inicial

--- week011/.eslintrc.js
+++ week011/.eslintrc.js
@@ -4,12 +4,29 @@ module.exports = {
     es2021: true,
     node: true,
   },
-  extends: ["eslint:recommended", "plugin:react/recommended"],
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:react/recommended",
+    "plugin:react-hooks/recommended",
+    "plugin:react/jsx-runtime",
+    "plugin:jsx-a11y/recommended",
+    "standard",
+    "prettier",
+  ],
   overrides: [],
   parserOptions: {
     ecmaVersion: "latest",
     sourceType: "module",
   },
-  plugins: ["react"],
-  rules: {},
+
+  plugins: ["react", "jsx-a11y"],
+  rules: {
+    "no-unused-vars": "warn",
+  },
+  settings: {
+    react: {
+      version: "detect",
+    },
+  },
 };
